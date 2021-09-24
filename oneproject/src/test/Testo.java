8f9e8aed4b0fc51a2e2a8c9a691a572c240d5ca1
amrit <amrit@192.168.2.80>
2021-09-23 22:52:54 -0400

    added start machine 1

--- oneproject/src/test/Testo.java
+++ oneproject/src/test/Testo.java
@@ -12,7 +12,7 @@ public class Testo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("start machine");
+		System.out.println("start machine 1");
 		
 		System.out.println(LocalDateTime.now());
 		
